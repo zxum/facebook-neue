2732b335c5ba15155592d384dd9f26dbe4960e75
zxum <zion.um@outlook.com>
2020-12-16 10:06:49 -0600

    Add Omniauth for Facebook login

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'google-cloud-storage', '~> 1.29', '>= 1.29.2', require: false
 
 gem 'figaro'
 
+gem 'omniauth-facebook'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :profiles
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:index, :show]
   resources :posts do
     resource :like
